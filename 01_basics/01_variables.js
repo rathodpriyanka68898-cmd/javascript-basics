0abf40fd9f52cf76690760fd801e1c70812bd2ef
rathodpriyanka68898-cmd <rathodpriyanka68898@gmail.com>
2025-12-25 09:59:13 +0530

    first commi

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 808897
+let accountEmail = "priya@google.com"
+var accountPassword = "67890"
+accountCity = "gulbarga"
+let accountState;
+
+//accountId = 2//not allowed
+
+accountEmail = "priya@rathod.com"
+accountPassword = "6362588"
+accountCity = "banglore"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
