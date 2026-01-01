0d2dcd699acf62a74d5d71e99fd4e5a3bdf6f4ad
rathodpriyanka68898-cmd <rathodpriyanka68898@gmail.com>
2026-01-01 19:23:14 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "priyanka"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//"99"=> 99
+//"99abc"=> NaN
+// true => 1; false => 0
+
+let isLoggedIn = "priyanka"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "priyanka" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
