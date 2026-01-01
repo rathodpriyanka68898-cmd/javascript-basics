1a4f0e979f0b372faf6fd1679a755a59b535bab5
rathodpriyanka68898-cmd <rathodpriyanka68898@gmail.com>
2026-01-01 23:09:14 +0530

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol,BigInt
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = symbol('123')
+const anotherId = symbol('123')
+ 
+console.log(id === anotherId);
+
+//const bigNumber = 4532657382636473n
+
+ 
+
+// Reference (non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj","dogo"]
+let myObj = {
+    name:"priyanka",
+    age: 18,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+console.log( typeof anotherId);
