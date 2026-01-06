3c490f147e2b28f194efd359f9905d43be159926
rathodpriyanka68898-cmd <rathodpriyanka68898@gmail.com>
2026-01-06 19:01:39 +0530

    Nums and Math

---
+++ Basic_01/06_nums_and_math.js
@@ -0,0 +1,42 @@
+const score = 600
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(1));
+
+const otherNumber = 25.8098
+
+//console.log(otherNumber.toPrecision(5));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString(en-IN));
+
+// ++++++++++++++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++++++++++++
+
+//console.log(Math);
+//console.log(Math.abs(-5));
+//console.log(Math.round(5.9));
+//console.log(Math.ceil(5.2));
+//console.log(Math.floor(4.2));
+//console.log(Math.min(5,6,8,9));
+//console.log(Math.max(6,7,8,9));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
+
+
